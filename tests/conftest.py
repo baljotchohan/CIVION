97e99307d7007c59c66e237b996718763779ea3c
baljotchohan <Jattbad328@gmail.com>
2026-03-09 14:16:50 +0530

    fix

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+import asyncio
+import pytest
+
+@pytest.fixture(autouse=True)
+def ensure_event_loop():
+    try:
+        asyncio.get_event_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
